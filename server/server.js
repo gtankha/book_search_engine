a76e9e7cbf04333116b7ee97a12019811ea4d426
gtankha <46304828+gtankha@users.noreply.github.com>
2020-12-21 00:48:11 -0800

    removed extra statement... deploying

--- server/server.js
+++ server/server.js
@@ -28,11 +28,6 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
  }
-
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
- }
  
 
 db.once('open', () => {
