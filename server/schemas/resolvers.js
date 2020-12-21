5b61dcacfd3578b7b3bc0d0c2c0e247e509e5515
gtankha <46304828+gtankha@users.noreply.github.com>
2020-12-21 01:47:10 -0800

    redeploying

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -54,7 +54,7 @@ const resolvers = {
                 const user = await User.findOneAndUpdate(
                     { _id: context.user._id },
                     { $addToSet: { savedBooks: body} },
-                    { new: true, runValidators: true}
+                    { new: true, runValidators: false}
                 );
 
                 return user;
